a1ee4d63f3389f8a9ff617254e2ff41863c126d0
Pau Fernández <pau.fernandez@upc.edu>
2012-08-17 17:03:15 +0200

    Server env variables

---
+++ server-env.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin
+export GOPATH=/usr/local/go:$HOME/go
+export ACADEMIO_PATH=$HOME/Academio
+export ACADEMIO_ROOT=$HOME/go/src/Academio/academio
